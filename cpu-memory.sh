58e2041032a040127232cdd69456539bdfdb509d
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2022-03-10 20:48:08 +0200

    Update cpu-memory.sh

--- cpu-memory.sh
+++ cpu-memory.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+cpu_top_10() {
+   echo "list of the top memory consumption"
+   ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 10   
+}
+
+mem_top_10() {
+   echo "list of the top cpu consumption"
+   ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 10
+}
+
+todo() {
+   echo "todo"
+   
+}
+
 while true
-   do
-        
+   do        
 	mem="$(free | grep Mem | awk '{print $3/$2 * 100.0}')"	
-  cpu="$(uptime | awk {'print $8'} | cut -d "," -f1)"
+        cpu="$(uptime | awk {'print $8'} | cut -d "," -f1)"
 
-	if (( $(echo "$mem > 75" |bc -l) )); then 			
-             echo "list of the top consumption processes"
-             ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 10
-        
+	if (( $(echo "$mem > 75" |bc -l) || $(echo "$cpu > 3" |bc -l) )); then 			
+           cpu_top_10 
+	   mem_top_10
+	   todo
         fi
-
-        if (( $(echo "$cpu > 2" |bc -l) )); then
-	     echo "list of the top cpu  processes"
-             ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 10
-        fi	
-
 	sleep 5
 
 done
